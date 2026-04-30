9f466025c476a1bf98c559cd3512baef540c0157
Von <sechetitouan@gmail.com>
2026-04-30 15:31:07 +0200

    parsing
    better path handling

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: tseche <tseche@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 17:43:17 by tseche            #+#    #+#             */
-/*   Updated: 2026/04/29 17:18:11 by pcaplat          ###   ########.fr       */
+/*   Updated: 2026/04/30 15:03:46 by tseche           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,29 +35,31 @@ typedef enum e_error_map
 	ERROR_OPEN,
 	ERROR_OPEN_XPM,
 	ERROR_MALLOC,
+	ERROR_PATH_TEXT,
 	ERROR_MAX,
 }		t_error_map;
 
 static const char	*g_errors[ERROR_MAX] = {
-[NO_ERROR] = ("WTF an error has been"
+("WTF an error has been"
 		"thrown, but no error was detected\n"),
-[INC_EXT] = "the map provided is not a .ber file\n",
-[INC_WALL] = "The map hasn't been properly walled\n",
-[INC_CHAR] = "An unrecognize character has been found\n",
-[INC_SHAPE] = "The Map is not a rectangle\n",
-[INV_MAP] = "the map is empty\n",
-[NOT_ENO_STRT] = "The Map contains no starting point\n",
-[TOO_MUCH_STRT] = "The map cointains too much starting point\n",
-[NOT_ENO_COLL] = "The Map contains no collectible\n",
-[NOT_ENO_FINISH] = "The Map contains no exit\n",
-[TOO_MUCH_FINISH] = "The map contain too much finish\n",
-[NOT_REACH_COLL] = "The Map contains a not reachable collectible\n",
-[NOT_REACH_FINISH] = "The Map contains a not reachable exit\n",
-[ERROR_SIZE] = "The map is limited to 100 * 100 duw to stack limitation\n",
-[ERROR_WIN] = "The map is limited due to screen size limitation\n",
-[ERROR_OPEN] = "A file couldn't be opened\n",
-[ERROR_OPEN_XPM] = "A XPM file couldn't be opened\n",
-[ERROR_MALLOC] = "Erreur malloc\n",
+"the map provided is not a .ber file\n",
+"The map hasn't been properly walled\n",
+"An unrecognize character has been found\n",
+"The Map is not a rectangle\n",
+"the map is empty\n",
+"The Map contains no starting point\n",
+"The map cointains too much starting point\n",
+"The Map contains no collectible\n",
+"The Map contains no exit\n",
+"The map contain too much finish\n",
+"The Map contains a not reachable collectible\n",
+"The Map contains a not reachable exit\n",
+"The map is limited to 100 * 100 duw to stack limitation\n",
+"The map is limited due to screen size limitation\n",
+"A file couldn't be opened\n",
+"A XPM file couldn't be opened\n",
+"The path of one the texture is invalid",
+"Erreur malloc\n",
 };
 // --- STRUCT ---
 
---
+++ srcs/parsing/path.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tseche <tseche@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/28 17:22:03 by tseche            #+#    #+#             */
+/*   Updated: 2026/04/30 15:30:12 by tseche           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+int	get_rel_path(char *line)
+{
+	int	len;
+	int	dot;
+	int	slash;
+
+	len = 0;
+	dot = 0;
+	slash = 0;
+	while (ft_isalpha(*line))
+	{
+		if (*line == '/' && slash == 0)
+			dot = 0;
+		else if (*line == '.' && dot <= 2)
+			dot++;
+		else if (*line == '/' || *line == '.')
+			return (-18);
+		else if (*line == '"' || *line == '\'')
+			return (-18);
+		line++;
+		len++;
+	}
+	return (len);
+}
+
+int	len_path(char *line, int quote[2])
+{
+	char	cpy;
+
+	cpy = line;
+	while (*line || *line == '\n')
+	{
+		if (*line == '(' || *line == ')' && quote[1])
+			break ;
+		else if (*line == '(' || *line == ')')
+			return (-18);
+		else if (*line == '\'' && (quote[1] == 0 || quote[0] == '\''))
+			return (-18);
+		else if (*line == '"' && (quote[1] == 0 || quote[0] == '"'))
+			return (-18);
+		line++;
+	}
+	return (line - cpy);
+}
+
+char *get_path(char *line)
+{
+	int		quote[2];
+	char	*str;
+	int		pre;
+	char	*cpy;
+	int		len;
+
+	cpy = line;
+	line += skip_spaces(line);
+	if (*line == '\'' || *line == '"'){
+		quote[1] = 1;
+		quote[0] = *line++;
+ 	}
+	pre = get_rel_path(line);
+	if (pre == -18)
+		return (NULL);
+	len = len_path(line, quote);
+	if (len == -18)
+		return (NULL);
+	str = ft_substr(cpy, 0, pre + line - cpy + len);
+	if (!str)
+		return (NULL);
+	return (str);
+}
