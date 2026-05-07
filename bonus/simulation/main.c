3ba6287e003563a68b1b7c5be1579a4e5c87a6af
Von <sechetitouan@gmail.com>
2026-05-07 18:35:42 +0200

    add spawn

--- bonus/simulation/main.c
+++ bonus/simulation/main.c
@@ -6,7 +6,7 @@
 /*   By: tseche <tseche@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 17:42:13 by tseche            #+#    #+#             */
-/*   Updated: 2026/05/07 17:56:53 by tseche           ###   ########.fr       */
+/*   Updated: 2026/05/07 18:35:07 by tseche           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int main()
 	const int	*range = get_range(1, NULL);
 	int			seed = gen_seed(range[0], range[1]);
 	t_map_simu	*map = seed_to_mapsimu(seed);
-	char		**map = simulate(map);
-	int			*spawn_point = spawn(map);
+	char		**nmap = simulate(map);
+	int			*spoint = spawn(nmap);
+	nmap[spoint[0]][spoint[1]] = "NSEW"[spoint[2]];
 }
