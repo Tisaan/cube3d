0661c567a7157868c71f8a4037ef9a90fd7313c1
pcaplat <pcaplat@42angouleme.fr>
2026-05-19 11:02:04 +0200

    add player radius constant

--- includes/player.h
+++ includes/player.h
@@ -6,7 +6,7 @@
 /*   By: pcaplat <pcaplat@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 16:01:24 by pcaplat           #+#    #+#             */
-/*   Updated: 2026/05/18 19:20:17 by pcaplat          ###   ########.fr       */
+/*   Updated: 2026/05/19 11:01:07 by pcaplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ typedef struct	s_player
 	t_vect		camera;
 	t_vect		pos;
 	t_vect		dir;
-	t_direction_id	facing;
 	int			size;
 	float		speed;
 	float		rot_speed;
