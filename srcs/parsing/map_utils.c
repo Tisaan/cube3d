658c11fb79bb6e93bf3942cce9929ffd4828bb32
Ix-Tap3 <patxicaplat.pro@gmail.com>
2026-06-01 14:31:44 +0200

    fix around invalid reads

--- srcs/parsing/map_utils.c
+++ srcs/parsing/map_utils.c
@@ -32,15 +32,15 @@ bool	around(t_map *map, int x, size_t y)
 		return (false);
 	else if (y == ft_strlen(map->grid[x]) || x == 0 || x == map->height)
 		return (false);
-	else if (x - 1 >= 0 && ft_isoneof(map->grid[x - 1][y], " \t\n"))
+	else if (x - 1 >= 0 && y <= ft_strlen(map->grid[x - 1]) && ft_isoneof(map->grid[x - 1][y], " \t\n"))
 		return (false);
-	else if (ft_isoneof(map->grid[x - 1][y - 1], " \t\n"))
+	else if (x - 1 >= 0 && y - 1 <= ft_strlen(map->grid[x - 1]) && ft_isoneof(map->grid[x - 1][y - 1], " \t\n"))
 		return (false);
 	else if ((int)(y + 1) * WALL_SIZE <= map->width && ft_isoneof(map->grid[x - 1][y + 1], " \t\n"))
 		return (false);
 	else if ((int)y * WALL_SIZE <= map->width && ft_isoneof(map->grid[x + 1][y], " \t\n"))
 		return (false);
-	else if (ft_isoneof(map->grid[x + 1][y - 1], " \t\n"))
+	else if (x + 1 <= map->height / WALL_SIZE && ft_isoneof(map->grid[x + 1][y - 1], " \t\n"))
 		return (false);
 	else if ((int)(y + 1) * WALL_SIZE <= map->width && ft_isoneof(map->grid[x + 1][y + 1], " \t\n"))
 		return (false);
