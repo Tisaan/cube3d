755365b3e46c1dbd01563f755cc0ccaf4fca0411
Von <118017851+Tisaan@users.noreply.github.com>
2026-06-02 15:59:41 +0200

    revert c de la merde

--- includes/vectors.h
+++ includes/vectors.h
@@ -3,26 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   vectors.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   By: tseche <tseche@student.42.fr>              +#+  +:+       +#+        */
+/*   By: von <von@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 18:51:18 by pcaplat           #+#    #+#             */
-/*   Updated: 2026/05/07 11:39:25 by tseche           ###   ########.fr       */
-=======
-/*   By: pcaplat <pcaplat@42angouleme.fr>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/06 18:51:18 by pcaplat           #+#    #+#             */
-/*   Updated: 2026/05/22 16:54:02 by pcaplat          ###   ########.fr       */
->>>>>>> parent of 75ab406 (conflict when merge seed)
-=======
-/*   By: pcaplat <pcaplat@42angouleme.fr>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/06 18:51:18 by pcaplat           #+#    #+#             */
-/*   Updated: 2026/05/22 16:54:02 by pcaplat          ###   ########.fr       */
->>>>>>> parent of 75ab406 (conflict when merge seed)
+/*   Updated: 2026/06/02 15:59:33 by von              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef VECTORS_H
 # define VECTORS_H
